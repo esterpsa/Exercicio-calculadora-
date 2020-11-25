16d53171ab0c0ea1e8e2bc8f04769dfca82403b8
Ester <ester.albuquerque500@gmail.com>
2020-11-25 11:22:33 -0300

    Exercio 1

---
+++ Condicional - exercio 1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Condicional___exercio_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade = int.Parse(Console.ReadLine());
+            int meses = (idade *12);
+            int dias = idade * 365;
+            int horas = dias * 24;
+            int minuto = horas * 60;
+
+            Console.WriteLine("digite a sua idade: ");
+            idade= int.Parse(Console.ReadLine());
+
+            System.Console.WriteLine("idade em meses:"+meses);
+            System.Console.WriteLine("idade em dias:"+dias);
+            System.Console.WriteLine("idade em horas:"+horas);
+            System.Console.WriteLine("idade em minutos:"+minuto); 
+
+
+        }
+    }
+}
